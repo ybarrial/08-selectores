3b97072a49e6699703f244d408a68abb79322c96
Your Name <yefferson.barrial@avivel.com.pe>
2024-02-04 18:53:30 -0500

    add getCountriesByRegion method in countries service

--- src/app/country/services/countries.service.ts
+++ src/app/country/services/countries.service.ts
@@ -1,16 +1,35 @@
 import { Injectable } from '@angular/core';
-import { Region } from '../interfaces/country.interfaces';
+
+import { Region, SmallCountry } from '../interfaces/country.interfaces';
+import { HttpClient } from '@angular/common/http';
+import { Observable, of, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CountriesService {
 
+  private baseUrl: string = 'https://restcountries.com/v3.1';
+
   private _regions: Region[] = [Region.Africa, Region.Amercias, Region.Asia, Region.Europe, Region.Oceania];
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   get regions(): Region[] {
     return [...this._regions];
   }
+
+  getCountriesByRegion(region: Region): Observable<SmallCountry[]> {
+    if( !region ) return of([]);
+
+    const url: string = `${this.baseUrl}/region/${region}?fields=cca3,name,borders`;
+
+    return this.http.get<SmallCountry[]>(url)
+      .pipe(
+        tap( response => console.log({ response }))
+      )
+  }
+
+
+
 }
