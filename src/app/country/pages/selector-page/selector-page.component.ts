15a9f27ad3c6a736a8e74c333db6f1aad1cd0fa9
Your Name <yefferson.barrial@avivel.com.pe>
2024-02-04 18:54:48 -0500

    add onRegionChanged() method subscribe

--- src/app/country/pages/selector-page/selector-page.component.ts
+++ src/app/country/pages/selector-page/selector-page.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { CountriesService } from '../../services/countries.service';
 import { Region } from '../../interfaces/country.interfaces';
+import { switchMap } from 'rxjs';
 
 @Component({
   selector: 'app-selector-page',
@@ -32,8 +33,11 @@ export class SelectorPageComponent implements OnInit {
 
   onRegionChanged(): void {
     this.myForm.get('region')?.valueChanges
-      .subscribe( region  => { 
-        console.log({ region });
+      .pipe(
+        switchMap( region => this.countriesService.getCountriesByRegion(region) )
+      )
+      .subscribe( countries  => { 
+        console.log({ countries });
       })
   }
 
