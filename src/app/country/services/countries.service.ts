4a29e75d72133c8f926d171e2126ecc0289e7db5
Your Name <yefferson.barrial@avivel.com.pe>
2024-02-05 22:52:53 -0500

    add  methods in countries service

--- src/app/country/services/countries.service.ts
+++ src/app/country/services/countries.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@angular/core';
-
-import { Region, SmallCountry } from '../interfaces/country.interfaces';
 import { HttpClient } from '@angular/common/http';
-import { Observable, of, tap } from 'rxjs';
+
+import { Country, Region, SmallCountry } from '../interfaces/country.interfaces';
+import { Observable, combineLatest, map, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -11,25 +11,56 @@ export class CountriesService {
 
   private baseUrl: string = 'https://restcountries.com/v3.1';
 
-  private _regions: Region[] = [Region.Africa, Region.Amercias, Region.Asia, Region.Europe, Region.Oceania];
+  private _regions: Region[] = [Region.Africa, Region.Americas, Region.Asia, Region.Europe, Region.Oceania];
 
-  constructor(private http: HttpClient) { }
+  constructor(
+    private http: HttpClient
+  ) { }
 
   get regions(): Region[] {
     return [...this._regions];
   }
 
   getCountriesByRegion(region: Region): Observable<SmallCountry[]> {
-    if( !region ) return of([]);
+    if (!region) return of([]);
 
     const url: string = `${this.baseUrl}/region/${region}?fields=cca3,name,borders`;
 
-    return this.http.get<SmallCountry[]>(url)
+    return this.http.get<Country[]>(url)
       .pipe(
-        tap( response => console.log({ response }))
+        map(countries => countries.map(country => ({
+          name: country.name.common,
+          cca3: country.cca3,
+          borders: country.borders ?? []
+        }))),
       )
   }
 
+  getCountryByAlphaCode(alphaCode: string): Observable<SmallCountry> {
+    const url = `${this.baseUrl}/alpha/${alphaCode}?fields=cca3,name,borders`;
+    return this.http.get<Country>(url)
+      .pipe(
+        map(country => ({
+          name: country.name.common,
+          cca3: country.cca3,
+          borders: country.borders ?? [],
+        }))
+      )
+  }
+
+  getCountryBordersByCodes(borders: string[]): Observable<SmallCountry[]> {
+    if (!borders || borders.length === 0) return of([]);
+
+    const countriesRequests: Observable<SmallCountry>[] = [];
+
+    borders.forEach(code => {
+      const request = this.getCountryByAlphaCode(code);
+      countriesRequests.push(request);
+    });
+
+
+    return combineLatest(countriesRequests);
+  }
 
 
 }
